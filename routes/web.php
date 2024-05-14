3cb995a82af22ed0699304ada9c7776f4b6454fc
Rizalfauzi13 <muhammadrizalfauzi@gmail.com>
2024-05-15 02:31:29 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//methode HTTP:
+// 1.Get di ggunakan unutk menampilkan data atau halamn 
+//2. Post digunakan untuk mengirim data 
+//Put digunkan untuk meng update data 
+//4 Delete di gunakan untk menghapus data 
+
+
+// dashboard
+Route::get('admin/dashboard',[DashboardController::class,'index']);
